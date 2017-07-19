affdd14a93600010c7a70919d2f74ab7d0e19af5
M V <mv@Ms-MacBook-Pro.local>
2017-07-18 21:10:59 -0400

    Added basic Spring Data

---
+++ cra-dao-impl/src/main/java/com/ihg/it/pfm/cradaoimpl/CraDaoImplApplication.java
@@ -0,0 +1,12 @@
+package com.ihg.it.pfm.cradaoimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraDaoImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraDaoImplApplication.class, args);
+	}
+}
---
+++ cra-dao-impl/src/test/java/com/ihg/it/pfm/cradaoimpl/CraDaoImplApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ihg.it.pfm.cradaoimpl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CraDaoImplApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
